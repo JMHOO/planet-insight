f6bede0466c75dd6c950f460c72a52c4b8c882dd
Jiaming <hujiaming@hotmail.com>
2017-10-04 14:09:07 -0700

    add bash script for starting restful using docker

---
+++ settings.py
@@ -0,0 +1,21 @@
+S3_BUCKET = {
+    'DATASET': 'insight-dataset',
+    'RESULTS': 'insight-results'
+}
+
+DynamoDB_TABLE = {
+    'JSONModel': 'insight-models',
+    'JobInstance': 'job-instance',
+    'InstanceLogs': 'instance-logs',
+    'Worker': 'workers'
+}
+
+DOCKER = {
+    'IMAGE': 'hjiaming/insight-worker',
+    'VERSION': 'latest'
+}
+
+MONITOR = {
+    'HOST': 'http://demo.umx.io',
+    'PATH': '/monitor'
+}
---
+++ start_restful_docker_service.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+aws_config=~/.aws
+if [ ! -d ${aws_config} ]; then
+  mkdir ${aws_config}
+fi
+
+# setting file inside the container
+setting_file=/home/root/insight/package/insight/applications/
+
+# external port
+ext_port=80
+
+docker run --rm -d --name insight-restful -v ${aws_config}:/root/.aws -v ${PWD}/settings.py:${setting_file} -p ${ext_port}:9000 hjiaming/insight-service

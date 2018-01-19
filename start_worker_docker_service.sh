2a80f0622af72fd56d6a8c133d29dd0699972150
Ryan Reece <ryan.reece@cern.ch>
2018-01-19 01:02:28 -0500

    update nvidia driver, settings.py, and add start_worker shell script

--- start_worker_docker_service.sh
+++ start_worker_docker_service.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [[ "$(docker images -q insight/tworker 2> /dev/null)" == "" ]]; then
+    docker build -t insight/tworker -f Dockerfile.worker .
+fi
+
 nvidia-docker run --rm -it --name insight --hostname $USER -v /var/run/docker.sock:/var/run/docker.sock -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} insight/tworker
 

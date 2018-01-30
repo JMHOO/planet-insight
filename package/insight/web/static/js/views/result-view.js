d2077082501914d7b01dd344d855c4c967279384
Ryan Reece <ryan.d.reece@live.com>
2018-01-29 20:06:41 -0800

    js work for results

--- package/insight/web/static/js/views/result-view.js
+++ package/insight/web/static/js/views/result-view.js
@@ -86,6 +86,39 @@ app.ResultView = Backbone.View.extend({
                 result_logs_group.append(output);
             }
         });
+
+        // Load the Visualization API and the corechart package.
+        google.charts.load('current', {'packages':['corechart']});
+
+        // Set a callback to run when the Google Visualization API is loaded.
+        google.charts.setOnLoadCallback(drawChart);
+
+        // Callback that creates and populates a data table,
+        // instantiates the pie chart, passes in the data and
+        // draws it.
+        function drawChart() {
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+              ['Mushrooms', 3],
+              ['Onions', 1],
+              ['Olives', 1],
+              ['Zucchini', 1],
+              ['Pepperoni', 2]
+            ]);
+
+            // Set chart options
+            var options = {'title':'How Much Pizza I Ate Last Night',
+                           'width':400,
+                           'height':300};
+
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('result_chart'));
+            chart.draw(data, options);
+        }
+
         $('#view-result-dialog').modal();
         $('#view-result-dialog').modal('handleUpdate');
     },

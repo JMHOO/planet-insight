9b2b1017368ac7beca44f56beb394c95316a20c8
Ryan Reece <ryan.d.reece@live.com>
2018-01-29 20:20:25 -0800

    js work for results

--- package/insight/web/static/js/views/result-view.js
+++ package/insight/web/static/js/views/result-view.js
@@ -88,7 +88,7 @@ app.ResultView = Backbone.View.extend({
         });
 
         // Load the Visualization API and the corechart package.
-        google.charts.load('current', {'packages':['corechart']});
+        google.charts.load('current', {'packages':['line']});
 
         // Set a callback to run when the Google Visualization API is loaded.
         google.charts.setOnLoadCallback(drawChart);
@@ -97,26 +97,41 @@ app.ResultView = Backbone.View.extend({
         // instantiates the pie chart, passes in the data and
         // draws it.
         function drawChart() {
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Topping');
-            data.addColumn('number', 'Slices');
-            data.addRows([
-              ['Mushrooms', 3],
-              ['Onions', 1],
-              ['Olives', 1],
-              ['Zucchini', 1],
-              ['Pepperoni', 2]
-            ]);
+			var data = new google.visualization.DataTable();
+      		data.addColumn('number', 'Day');
+      		data.addColumn('number', 'Guardians of the Galaxy');
+      		data.addColumn('number', 'The Avengers');
+      		data.addColumn('number', 'Transformers: Age of Extinction');
 
-            // Set chart options
-            var options = {'title':'How Much Pizza I Ate Last Night',
-                           'width':400,
-                           'height':300};
+      		data.addRows([
+      		  [1,  37.8, 80.8, 41.8],
+      		  [2,  30.9, 69.5, 32.4],
+      		  [3,  25.4,   57, 25.7],
+      		  [4,  11.7, 18.8, 10.5],
+      		  [5,  11.9, 17.6, 10.4],
+      		  [6,   8.8, 13.6,  7.7],
+      		  [7,   7.6, 12.3,  9.6],
+      		  [8,  12.3, 29.2, 10.6],
+      		  [9,  16.9, 42.9, 14.8],
+      		  [10, 12.8, 30.9, 11.6],
+      		  [11,  5.3,  7.9,  4.7],
+      		  [12,  6.6,  8.4,  5.2],
+      		  [13,  4.8,  6.3,  3.6],
+      		  [14,  4.2,  6.2,  3.4]
+      		]);
 
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.PieChart(document.getElementById('result_chart'));
-            chart.draw(data, options);
+      		var options = {
+      		  chart: {
+      		    title: 'Box Office Earnings in First Two Weeks of Opening',
+      		    subtitle: 'in millions of dollars (USD)'
+      		  },
+      		  width: 900,
+      		  height: 500
+      		};
+
+      		var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
+      		chart.draw(data, google.charts.Line.convertOptions(options));
         }
 
         $('#view-result-dialog').modal();

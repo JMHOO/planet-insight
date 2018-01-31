9281a45fe20aa5b1153d8f15479294f7976b271f
Ryan Reece <ryan.d.reece@live.com>
2018-01-31 14:30:35 -0800

    REST for downloading datasets

--- package/insight/web/REST_server.py
+++ package/insight/web/REST_server.py
@@ -408,6 +408,7 @@ def upload_weights():
 @checkAWS
 def get_dataset(dataset_name):
 #    return {"file": dataset_name}
+    dataset_name = 'dataset/%s' % dataset_name
     url = aws.datasets.presigned_url(dataset_name)
     print('get_dataset') # DEBUG
     print(url)

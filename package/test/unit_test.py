b001562aa33f7899b768694644139c2c700b223c
Ryan Reece <ryan.reece@cern.ch>
2018-02-13 19:20:18 -0500

    new models

--- package/test/unit_test.py
+++ package/test/unit_test.py
@@ -102,12 +102,20 @@ def test_json_build_from_string():
     ]
     '''
 
+    cifar100_coarse_json = '''[
+        { "From": "CNN-Base" },
+        { "Dense": { "units": 20, "activation": "softmax", "name": "softmax1" }, "input": "dropout3" },
+        { "Compiler": { "optimizer": { "adam": {"lr": 0.0001} }, "loss": "categorical_crossentropy", "metrics": ["accuracy"] } } 
+    ]
+    '''
+
     c = Convert()
     #keras_model = c.parser(example_json)
     #keras_model = c.parser(cut_json, example_json)
     #keras_model = c.parser(cut_json, example_json, hparams={"conv2/filters":32})
     #keras_model = c.parser(optimizer_json, example_json)
-    keras_model = c.parser(cifar100_json, example_json)
+    #keras_model = c.parser(cifar100_json, example_json)
+    keras_model = c.parser(cifar100_coarse_json, example_json)
     print(keras_model)
 
     return keras_model
@@ -152,6 +160,13 @@ def test_dynamodb():
     ]
     '''
 
+    cifar100_coarse_json = '''[
+        { "From": "CNN-Base" },
+        { "Dense": { "units": 20, "activation": "softmax", "name": "softmax1" }, "input": "dropout3" },
+        { "Compiler": { "optimizer": { "adam": {"lr": 0.0001} }, "loss": "categorical_crossentropy", "metrics": ["accuracy"] } } 
+    ]
+    '''
+
     ryan_json = '''[{
         "Convolution2D": {"inputs": [null, 32, 32, 3],"filters": 64, "kernel_size": [3, 3],"strides": [1, 1],"activation": "relu","padding": "valid",
             "kernel_initializer": {
@@ -179,17 +194,37 @@ def test_dynamodb():
     ]
     '''
 
-#    db_model.put('CNN-Ryan', ryan_json)
-#    json_str = db_model.get('CNN-Ryan')
-#    j = json.loads(json_str)
-#    print(j)
-#    print(type(j))
+    mnist_json = '''[{
+        "Convolution2D": {"inputs": [null, 28, 28, 1],"filters": 32, "kernel_size": [3, 3],"strides": [1, 1],"activation": "relu","padding": "valid",
+            "kernel_initializer": {
+                "VarianceScaling": {"scale": 1,"mode": "fan_avg","distribution": "uniform"}
+            },"name": "conv1"}
+        },
+        { "Convolution2D": { "filters": 64, "kernel_size": [3, 3], "strides": [1, 1], "activation": "relu", "padding": "valid", "name": "conv2" } },
+        { "MaxPooling2D": { "pool_size": [2, 2], "strides": [2, 2], "padding": "valid", "name": "pool1" } },
+        { "Dropout": { "rate": 0.25, "name": "dropout1" } },
+        { "Convolution2D": { "filters": 128, "kernel_size": [3, 3], "strides": [1, 1], "activation": "relu", "padding": "valid", "name": "conv3" } },
+        { "MaxPooling2D": { "pool_size": [2, 2], "strides": [2, 2], "padding": "valid", "name": "pool2" } },
+        { "Convolution2D": { "filters": 128, "kernel_size": [3, 3], "strides": [1, 1], "activation": "relu", "padding": "valid", "name": "conv4" } },
+        { "MaxPooling2D": { "pool_size": [2, 2], "strides": [2, 2], "padding": "valid", "name": "pool3" } },
+        { "Dropout": { "rate": 0.25, "name": "dropout2" } },
+        { "Flatten": {} },
+        { "Dense": { "units": 1024, "activation": "relu", "name": "dense1" } },
+        { "Dropout": { "rate": 0.5, "name": "dropout3" } },
+        { "Dense": { "units": 10, "activation": "softmax", "name": "softmax1" } }
+    ]'''
 
-    db_model.put('CNN-Ryan-100', ryan100_json)
-    json_str = db_model.get('CNN-Ryan-100')
+    db_model.put('CNN-MNIST', mnist_json)
+    json_str = db_model.get('CNN-MNIST')
     j = json.loads(json_str)
     print(j)
     print(type(j))
+
+#    db_model.put('CNN-Ryan-100', ryan100_json)
+#    json_str = db_model.get('CNN-Ryan-100')
+#    j = json.loads(json_str)
+#    print(j)
+#    print(type(j))
 
     # jobInstances = DBJobInstance()
     # jobInstances.new_job({
